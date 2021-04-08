bedc2f3ba605557e069c174a532753ff60259d68
Olivier <omaffeis@hotmail.com>
2021-04-08 11:44:12 +0200

    Add main unit test

---
+++ lib/src/test/java/com/m2i/devops/applicationTest.java
@@ -0,0 +1,16 @@
+package com.m2i.devops;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class applicationTest {
+
+	@Test
+	public void testMain() {
+		application app = new application();
+		// app.main({'a','b'});
+		assertTrue(true); 
+	}
+
+}
