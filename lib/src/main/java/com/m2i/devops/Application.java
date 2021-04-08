589df4b14e2d835ab51ba34f821194e6e45e06b2
Olivier <omaffeis@hotmail.com>
2021-04-08 16:29:57 +0200

    Fix checkstyle warnings

--- lib/src/main/java/com/m2i/devops/Application.java
+++ lib/src/main/java/com/m2i/devops/Application.java
@@ -2,17 +2,17 @@ package com.m2i.devops;
 
 public class Application {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello W");
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Hello W");
 
-		Calculator calc = new Calculator();
-		System.out.println("Add 2+3 : " + calc.add( 2, 3));
-		System.out.println("Sub 2-3 : " + calc.sub( 2, 3));
-		System.out.println("IsEven 4  : " + calc.isEven(4));
+    Calculator calc = new Calculator();
+    System.out.println("Add 2+3 : " + calc.add( 2, 3));
+    System.out.println("Sub 2-3 : " + calc.sub( 2, 3));
+    System.out.println("IsEven 4  : " + calc.isEven(4));
 
-		System.out.println("Prime numbers from 1 to 100 are :" + calc.getPrimeNumber(15));
+    System.out.println("Prime numbers from 1 to 100 are :" + calc.getPrimeNumber(15));
 
-	}
+  }
 
 }
--- lib/src/main/java/com/m2i/devops/Calculator.java
+++ lib/src/main/java/com/m2i/devops/Calculator.java
@@ -1,49 +1,49 @@
 package com.m2i.devops;
 
 public class Calculator {
-	public int  add(int a, int b) {
-		return a+b;
-	}
+  public int  add(int a, int b) {
+    return a+b;
+  }
 
-	public int  mult(int a, int b) {
-		return a*b;
-	}
+  public int  mult(int a, int b) {
+    return a*b;
+  }
 
-	public int sub(int a, int b) {
-		return a-b;
-	}
+  public int sub(int a, int b) {
+    return a-b;
+  }
 
-	public int mod(int a, int b) {
-		return a%b;
-	}
-	
-	public boolean isEven(int a) {
-		return mod(a, 2) == 0;
-	}
-	
-	public String getPrimeNumber(int max)
-	{		
-		int i =0;
-		int num =0;
-		//Empty String
-		String  primeNumbers = "";
+  public int mod(int a, int b) {
+    return a%b;
+  }
 
-		for (i = 1; i <= max; i++)         
-		{ 		  	  
-			int counter=0; 	  
-			for(num =i; num>=1; num--)
-			{
-				if(i%num==0)
-				{
-					counter = counter + 1;
-				}
-			}
-			if (counter ==2)
-			{
-				//Appended the Prime number to the String
-				primeNumbers = primeNumbers + i + " ";
-			}
-		}
-		return primeNumbers;
-	}
+  public boolean isEven(int a) {
+    return mod(a, 2) == 0;
+  }
+
+  public String getPrimeNumber(int max)
+  {		
+    int i =0;
+    int num =0;
+    //Empty String
+    String  primeNumbers = "";
+
+    for (i = 1; i <= max; i++)         
+    { 		  	  
+      int counter=0; 	  
+      for(num =i; num>=1; num--)
+      {
+        if(i%num==0)
+        {
+          counter = counter + 1;
+        }
+      }
+      if (counter ==2)
+      {
+        //Appended the Prime number to the String
+        primeNumbers = primeNumbers + i + " ";
+      }
+    }
+    return primeNumbers;
+  }
 }
